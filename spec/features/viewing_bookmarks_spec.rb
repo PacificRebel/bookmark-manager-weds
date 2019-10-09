1685fe529db98e497df8ae129d4c38a2d82eb510
Heli Sivunen <heli@mooncat.tv>
2019-10-09 10:49:03 +0100

    Add ability to add bookmarks. Add 'create' method and refactor feature test accordingly.

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -4,10 +4,13 @@ feature 'Viewing the list of Bookmarks' do
   scenario 'displays Bookmarks' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
 
-    # add the test data
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.twitter.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    # add the test data - old code below, before it was refactored
+    # connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    # connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.twitter.com');")
+    # connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    Bookmark.create(url: "http://www.makersacademy.com")
+    Bookmark.create(url: "http://www.google.com")
+    Bookmark.create(url: "http://www.twitter.com")    
 
     visit ('/bookmarks')
 
